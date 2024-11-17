32f33477d7c79e62508bb93579aacbf32b13802e
muathzoubi <moathaza@gmail.com>
2024-11-17 10:16:53 +0300

    sad

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import {
-  createTheme,
-  StyledEngineProvider,
-  ThemeProvider,
-} from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShortLinkGenerator from './App';
 
@@ -16,30 +9,11 @@ const root = ReactDOM.createRoot(rootElement!);
 
 // All `Portal`-related components need to have the the main app wrapper element as a container
 // so that the are in the subtree under the element used in the `important` option of the Tailwind's config.
-const theme = createTheme({
-  cssVariables: true,
-  components: {
-    MuiPopover: {
-      defaultProps: {
-        container: rootElement,
-      },
-    },
-    MuiPopper: {
-      defaultProps: {
-        container: rootElement,
-      },
-    },
-  },
-});
+
 
 root.render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
         <ShortLinkGenerator />
-      </ThemeProvider>
-    </StyledEngineProvider>
   </React.StrictMode>
 );
 
